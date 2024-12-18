8d6d6da86bdf5b98654c5cafb526b54185b601f7
Aiken Geln <navtechng@outlook.com>
2024-12-18 01:59:17 +0100

    Added listViewBuilder.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,38 +5,27 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> items = ['Item1', 'Item2', 'Item3'];
+
     return MaterialApp(
-      title: 'List',
+      title: 'Example',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('List View'),
           backgroundColor: Colors.blue,
           titleTextStyle: TextStyle(color: Colors.orange[50]),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Person 1'),
-              subtitle: const Text('emain@one.com'),
-              trailing: const Icon(Icons.arrow_forward),
-              onTap: () => print('ListTile 1 was tapped'),
-            ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Person 2'),
-              subtitle: const Text('emain@two.com'),
-              trailing: const Icon(Icons.arrow_forward),
-              onTap: () => print('ListTile 2 was tapped'),
-            ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Person 3'),
-              subtitle: const Text('emain@three.com'),
-              trailing: const Icon(Icons.arrow_forward),
-              onTap: () => print('ListTile 3 was tapped'),
-            ),
-          ],
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return ListTile(
+                title: Text(item),
+                onTap: () {
+                  print('You just tapped on $item');
+                });
+          },
         ),
       ),
     );
