7ee91a332e5fad49389dc3216cb0549e55711d37
Aiken Geln <navtechng@outlook.com>
2024-12-18 01:40:51 +0100

    Added listView.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,71 +6,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dialog Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // INITIAL VAR --.> String
-  String _inputText = '';
-
-  // void show input dialog
-
-  // AlertDialog
-  void _showInputDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          // manage the text input field // controller.text
-          TextEditingController controller = TextEditingController();
-
-          return AlertDialog(
-            title: const Text('Enter some Text:'),
-            content: TextField(
-              controller: controller,
-              decoration: const InputDecoration(hintText: 'Enter some text'),
+      title: 'List',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('List View'),
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(color: Colors.orange[50]),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Person 1'),
+              subtitle: const Text('emain@one.com'),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () => print('ListTile 1 was tapped'),
             ),
-            actions: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Cancel')),
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _inputText = controller.text;
-                    });
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Save'))
-            ],
-          );
-        });
-  }
-
-  // appBar - body (updated State text) -- Icon
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My New App!'),
-        backgroundColor: Colors.blue,
-        titleTextStyle: TextStyle(color: Colors.orange[50]),
-      ),
-      body: Center(
-        child: Text(_inputText.isEmpty ? 'Tap icon to enter text' : _inputText),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showInputDialog,
-        child: const Icon(Icons.edit),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Person 2'),
+              subtitle: const Text('emain@two.com'),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () => print('ListTile 2 was tapped'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Person 3'),
+              subtitle: const Text('emain@three.com'),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () => print('ListTile 3 was tapped'),
+            ),
+          ],
+        ),
       ),
     );
   }
